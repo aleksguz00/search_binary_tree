586942f46b04b4ee44b878bca6aa20973b8de0e3
Alex <alexguz8279@gmail.com>
2024-05-14 19:48:06 +0500

    Changed Insert methods, Added Print method

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ private:
     std::unique_ptr<Node<Value>> left_;
     std::unique_ptr<Node<Value>> right_;
 
-    friend BinaryTree<Node<Value>>;
+    friend BinaryTree<Value>;
 };
 
 template <class Value>
@@ -49,33 +49,52 @@ public:
         root_{ std::make_unique<Node<Value>>(data) } {}
 
     void Insert(Value data) {
-        root_ = InsertNode_(root_, data);
+        InsertNode_(root_, data);
+    }
+
+    void Print() const {
+        PrintTree_(root_);
+        std::cout << '\n';
     }
 
 private:
     std::unique_ptr<Node<Value>> root_;
 
-    std::unique_ptr<Node<Value>> InsertNode_(
-        std::unique_ptr<Node<Value>> node,
+    void InsertNode_(
+        std::unique_ptr<Node<Value>>& node,
         Value data
     ) {
         if (!node) {
-            return std::make_unique<Node<Value>>(data);
+            node = std::make_unique<Node<Value>>(data);
+            return;
         }
 
         if (data < node->data_) {
-            node->left_ = InsertNode_(node->left_, data);
+            InsertNode_(node->left_, data);
         }
         else {
-            node->right_ = InsertNode_(node->right_, data);
+            InsertNode_(node->right_, data);
         }
+    }
 
-        return node;
+    void PrintTree_(const std::unique_ptr<Node<Value>>& node) const {
+        if (node) {
+            PrintTree_(node->left_);
+            std::cout << node->data_ << " ";
+            PrintTree_(node->right_);
+        }
     }
 };
 
 int main() {
+    BinaryTree<int> tree;
+    tree.Insert(5);
+    tree.Insert(3);
+    tree.Insert(7);
+    tree.Insert(1);
+    tree.Insert(777);
 
+    tree.Print();
 }
 
 
