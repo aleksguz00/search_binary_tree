a843d25c86643e82662da7dd51f743e538f87f6c
Alex <alexguz8279@gmail.com>
2024-05-14 19:34:28 +0500

    Completely redesign binary tree realization. Added ctors to both classes, private and public methods for BinaryTree class

--- main.cpp
+++ main.cpp
@@ -5,63 +5,155 @@
 template<class Value>
 class BinaryTree;
 
-template<class Value>
-class TreeNode {
+template <class Value>
+class Node {
 public:
-    TreeNode() = delete;
-    explicit TreeNode(Value value) : value_{ value }, left_{ nullptr }, right_{ nullptr } {}
+    Node() = delete;
 
-    TreeNode(const TreeNode<Value>& node) : 
-            value_{ node.value_ }, left_{ nullptr }, right_{ nullptr } {
-                if (node.left_) {
-                    left_ = std::make_unique<TreeNode<Value>>(*node.left_);
-                }
+    explicit Node(Value data) : 
+        data_{ data }, left_{ nullptr }, right_{ nullptr } {}
 
-                if (node.right_) {
-                    right_ = std::make_unique<TreeNode<Value>>(*node.right_);
-                }
+    Node(const Node& other) : 
+        data_{ other.data_ }, left_{ nullptr }, right_{ nullptr } {
+            if (other.left_) { 
+                left_ = other.left_.release();
             }
 
-    TreeNode(TreeNode<Value>&& node) noexcept :
-            value_{ std::move(node.value_ ) }, 
-            left_{ std::move(node.left_) },
-            right_{ std::move(node.right_) } {}
+            if (other.right_) {
+                right_ = other.left_.release();
+            }
+        }
+
+    Node(Node&& other) : 
+        data_{ std::move(other.data_) },
+        left_{ std::move(other.left_) },
+        right_{ std::move(other.right_) } {}
 
 private:
-    Value value_;
-    std::unique_ptr<TreeNode<Value>> left_;
-    std::unique_ptr<TreeNode<Value>> right_;
+    Value data_;
+    std::unique_ptr<Node<Value>> left_;
+    std::unique_ptr<Node<Value>> right_;
 
-    friend BinaryTree<Value>;
+    friend BinaryTree<Node<Value>>;
 };
 
-template<class Value>
+template <class Value>
 class BinaryTree {
 public:
     BinaryTree() = default;
-    explicit BinaryTree(TreeNode<Value> node) : root_{ node } {}
-    explicit BinaryTree(std::initializer_list<Value> values) {}
-    explicit BinaryTree(std::initializer_list<TreeNode<Value>> nodes) {}
-    
-    void Insert(TreeNode<Value> node) {
-        if (root_ == nullptr) {
-            root_ = std::make_unique<TreeNode<Value>>(node);
-            ++size_;
-            return;
-        }
 
+    explicit BinaryTree(Node<Value> node) :
+        root_{ std::make_unique<Node<Value>>(node) } {}
 
+    explicit BinaryTree(Value data) :
+        root_{ std::make_unique<Node<Value>>(data) } {}
+
+    void Insert(Value data) {
+        root_ = InsertNode_(root_, data);
     }
 
 private:
-    std::unique_ptr<TreeNode<Value>> root_;
-    size_t size_ = 0;
+    std::unique_ptr<Node<Value>> root_;
 
+    std::unique_ptr<Node<Value>> InsertNode_(
+        std::unique_ptr<Node<Value>> node,
+        Value data
+    ) {
+        if (!node) {
+            return std::make_unique<Node<Value>>(data);
+        }
 
+        if (data < node->data_) {
+            node->left_ = InsertNode_(node->left_, data);
+        }
+        else {
+            node->right_ = InsertNode_(node->right_, data);
+        }
+
+        return node;
+    }
 };
 
 int main() {
-    TreeNode<int> node{ 5 };
-    BinaryTree<int> tree;
-    tree.Insert(node);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// struct Node {
+//     int data;
+//     Node *left;
+//     Node *right;
+// };
+
+// class BinarySearchTree {
+// private:
+//     Node *root;
+
+//     Node* insertNode(Node *node, int data) {
+//         if (node == nullptr) {
+//             return new Node{data, nullptr, nullptr};
+//         } else {
+//             if (data <= node->data) {
+//                 node->left = insertNode(node->left, data);
+//             } else {
+//                 node->right = insertNode(node->right, data);
+//             }
+//         }
+
+//         return node;
+//     }
+
+//     void printTree(Node *node) {
+//         if (node != nullptr) {
+//             printTree(node->left);
+//             cout << node->data << " ";
+//             printTree(node->right);
+//         }
+//     }
+
+// public:
+//     BinarySearchTree() : root(nullptr) {}
+
+//     void insert(int data) {
+//         root = insertNode(root, data);
+//     }
+
+//     void printInOrder() {
+//         printTree(root);
+//         cout << endl;
+//     }
+// };
+
+// int main() {
+//     BinarySearchTree bst;
+
+//     bst.insert(10);
+//     bst.insert(5);
+//     bst.insert(15);
+//     bst.insert(3);
+//     bst.insert(7);
+
+//     cout << "In-order traversal: ";
+//     bst.printInOrder();
+
+//     return 0;
+// }
